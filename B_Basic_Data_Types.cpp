09fe6467eda65dbc8b4f658d41313c13905c0ae4
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-20 16:33:31 +0600

    Solved : B_Basic_Data_Types

---
+++ B_Basic_Data_Types.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // inisialize variables
+    int n;
+    long long n1;
+    char s;
+    float n2;
+    double n3;
+
+    // take input all variables
+    cin >> n >> n1 >> s >> n2 >> n3;
+
+    // print all variable values
+    cout << n << endl;
+    cout << n1 << endl;
+    cout << s << endl;
+    cout << n2 << endl;
+    cout << n3 << endl;
+
+    return 0;
+}
