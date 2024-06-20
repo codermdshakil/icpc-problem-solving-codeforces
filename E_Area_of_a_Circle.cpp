0fae90a39b60e55927ff71e4db0876ae98db765e
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-20 16:47:41 +0600

    Solved : E_Area_of_a_Circle

---
+++ E_Area_of_a_Circle.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <ios>
+using namespace std;
+
+int main()
+{
+
+    double r = 3.141592653;
+    double x;
+    cin >> x;
+    double result = r * (x * 2);
+
+    cout << fixed << setprecision(9) << endl;
+    cout << result<< endl;
+
+    return 0;
+}
