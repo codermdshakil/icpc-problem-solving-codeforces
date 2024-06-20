e00e4738793d9a0c19b305ceda19c686646a062a
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-20 17:37:49 +0600

    Solved : G_Summation_from_1_to_N

---
+++ G_Summation_from_1_to_N.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long  n;
+    // take input of n
+    cin >> n;
+    // 1 to n sum formula
+    long long result = n * (n + 1) / 2;
+    cout << result << endl;
+
+    return 0;
+}
